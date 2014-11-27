1603257b5af3e13cf52b9334b88e1fcb5ec6b5b7
ZHANG Nan <zhn.i@hotmail.com>
2014-11-27 01:12:13 +0100

    init project

---
+++ Infomaniak/Infomaniak/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Infomaniak
+//
+//  Created by ZN-MAC on 14/11/27.
+//  Copyright (c) 2014年 ZHANG Nan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
